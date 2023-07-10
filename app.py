4d4e9dba7bc419b1ee056412c99c3ab44aef2f8f
Plolw <115927381+Plolw@users.noreply.github.com>
2023-07-10 19:18:16 +0200

    mas cambios

--- app.py
+++ app.py
@@ -11,7 +11,19 @@ app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///web.db'
+db = SQLAlchemy(app)
 Session(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
 
 @app.after_request
 def after_request(response):
@@ -24,7 +36,7 @@ def after_request(response):
 @app.route("/")
 @login_required
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -33,6 +45,7 @@ def login():
     session.clear()
 
     if request.method == "POST":
+
         return render_template("index.html")
     else:
         return render_template("login.html")
