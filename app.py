ce0078d9a879b0775e2af2373a64afced114b28d
Pol Ortiz <polpolortiz@gmail.com>
2023-07-10 11:46:40 +0200

    test

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
 
 
 
@@ -20,3 +21,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+@app.route("/")
+def hello():
+    return "Hello World!"
