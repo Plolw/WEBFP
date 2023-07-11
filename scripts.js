7830f1fd6e4e73766ac70fee2df5fac7c49f5c2f
Plolw <115927381+Plolw@users.noreply.github.com>
2023-07-11 11:39:10 +0200

    ff

--- scripts.js
+++ scripts.js
@@ -2,10 +2,13 @@ function check(){
     if (document.getElementById('password').value == document.getElementById('confirm_password').value) {
       document.getElementById('message').style.color = 'green';
       document.getElementById('message').innerHTML = 'matching';
+      document.getElementById('validation').disabled = false;
+      
     } else {
       document.getElementById('message').style.color = 'red';
       document.getElementById('message').innerHTML = 'not matching';
+      document.getElementById('validation').disabled = true;
     }
   }
 
-  setInterval(check, 10);
+setInterval(check, 10);
